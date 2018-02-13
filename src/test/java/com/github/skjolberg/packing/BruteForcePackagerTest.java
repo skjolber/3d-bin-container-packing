2985735d8ffa2ca6ca2aa737f610a98c548ad453
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2018-02-13 22:11:51 +0100

    add simple performance plot

--- src/test/java/com/github/skjolberg/packing/BruteForcePackagerTest.java
+++ src/test/java/com/github/skjolberg/packing/BruteForcePackagerTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BruteForcePackagerTest extends AbstractPackagerTest {
@@ -33,7 +34,7 @@ public class BruteForcePackagerTest extends AbstractPackagerTest {
 		print(fits1);
  		assertNull(packager.pack(products1));
 	}
-
+	
 	@Test
 	public void testRunsFor5Seconds() {
 		List<Box> containers = new ArrayList<Box>();
@@ -54,6 +55,43 @@ public class BruteForcePackagerTest extends AbstractPackagerTest {
 		Container fits1 = bruteForcePackager.pack(products1, System.currentTimeMillis() + 5000);
 		assertNull(fits1);
 		assertTrue(System.currentTimeMillis() - time >= 5000);
+	}
+
+	@Test
+	@Ignore("Run manually")
+	public void testRunsPerformanceGraphLinearStacking() {
+		long duration = 120;
+		
+		System.out.println("Run for " + duration + " seconds");
+		
+		long deadline = System.currentTimeMillis() + duration * 1000;
+		int n = 1;
+		while(deadline > System.currentTimeMillis()) {
+			List<Box> containers = new ArrayList<Box>();
+			containers.add(new Box(5 * n, 10, 10));
+			Packager bruteForcePackager = new BruteForcePackager(containers, true);
+			
+			List<Box> products1 = new ArrayList<Box>();
+
+			for(int i = 0; i < n; i++) {
+				Box box = new Box(Integer.toString(i), 5, 10, 10);
+				for(int k = 0; k < i % 2; k++) {
+					box.rotate3D();
+				}
+				products1.add(box);
+			}
+
+			long time = System.currentTimeMillis();
+			Container container = bruteForcePackager.pack(products1, deadline);
+			if(container != null) {
+				System.out.println(n + " in " + (System.currentTimeMillis() - time));
+			} else {
+				System.out.println(n + " discarded");
+			}
+			
+			n++;
+		}
+		
 	}
 
 }
