e198d3d2809ad259c4395c0b94c6dc4ecbe8641f
Michel Daviot <michel@colisweb.com>
2018-09-14 10:53:42 +0200

    fixed typo

--- src/main/java/com/github/skjolberg/packing/Level.java
+++ src/main/java/com/github/skjolberg/packing/Level.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 
 /**
  * A level within a container
- * 
+ *
  */
 
 public class Level extends ArrayList<Placement>{
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public int getHeight() {
 		int height = 0;
-		
+
 		for(Placement placement : this) {
 			Box box = placement.getBox();
 			if(box.getHeight() > height) {
 				height = box.getHeight();
 			}
 		}
-		
+
 		return height;
 	}
 
@@ -35,25 +35,25 @@ public class Level extends ArrayList<Placement>{
 	}
 
 	/**
-	 * 
+	 *
 	 * Check whether placement is valid, i.e. no overlaps.
-	 * 
+	 *
 	 */
-	
+
 	public void validate() {
 		for(int i = 0; i < size(); i++) {
 			for(int j = 0; j < size(); j++) {
 				if(j == i) {
-					if(!get(i).intercets(get(j))) {
+					if(!get(i).intersects(get(j))) {
 						throw new IllegalArgumentException();
 					}
 				} else {
-					if(get(i).intercets(get(j))) {
+					if(get(i).intersects(get(j))) {
 						throw new IllegalArgumentException(i + " vs " + j);
 					}
 				}
 			}
-		}		
+		}
 	}
-	
+
 }
--- src/main/java/com/github/skjolberg/packing/Placement.java
+++ src/main/java/com/github/skjolberg/packing/Placement.java
@@ -1,25 +1,25 @@
 package com.github.skjolberg.packing;
 
 /**
- * Placement as in box in a space. 
- * 
- * The box does not necessarily fill the whole space. 
+ * Placement as in box in a space.
+ *
+ * The box does not necessarily fill the whole space.
  */
 
 public class Placement {
 
 	private Space space;
 	private Box box;
-	
+
 	public Placement(Space space, Box box) {
 		this.space = space;
 		this.box = box;
 	}
-	
+
 	public Placement(Space space) {
 		this.space = space;
 	}
-	
+
 	public Space getSpace() {
 		return space;
 	}
@@ -32,54 +32,54 @@ public class Placement {
 	public void setBox(Box box) {
 		this.box = box;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Placement [" + space.getX() + "x" + space.getY() + "x" + space.getZ() + ", width=" + box.getWidth() + ", depth=" + box.getDepth()+ ", height="
 				+ box.getHeight()+ "]";
 	}
-	
+
 	public int getCenterX() {
 		return space.getX() + (box.getWidth() / 2);
 	}
-	
+
 	public int getCenterY() {
 		return space.getY() + (box.getDepth() / 2);
 	}
-	
-	public boolean intercets(Placement placement) {
-		return intercetsX(placement) && intercetsY(placement);
+
+	public boolean intersects(Placement placement) {
+		return intersectsX(placement) && intersectsY(placement);
 	}
-	
-	public boolean intercetsY(Placement placement) {
-		
+
+	public boolean intersectsY(Placement placement) {
+
 		int startY = space.getY();
 		int endY = startY + box.getDepth() - 1;
 
 		if(startY <= placement.getSpace().getY() && placement.getSpace().getY() <= endY) {
 			return true;
 		}
-		
+
 		if(startY <= placement.getSpace().getY() + placement.getBox().getDepth() - 1 && placement.getSpace().getY() + placement.getBox().getDepth() - 1 <= endY) {
 			return true;
 		}
 
 		return false;
 	}
-	
-	public boolean intercetsX(Placement placement) {
-		
+
+	public boolean intersectsX(Placement placement) {
+
 		int startX = space.getX();
 		int endX = startX + box.getWidth() - 1;
 
 		if(startX <= placement.getSpace().getX() && placement.getSpace().getX() <= endX) {
 			return true;
 		}
-		
+
 		if(startX <= placement.getSpace().getX() + placement.getBox().getWidth() - 1 && placement.getSpace().getX() + placement.getBox().getWidth() - 1  <= endX) {
 			return true;
 		}
-		
+
 		return false;
 	}
 }
