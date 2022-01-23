ed2e08a1a4b4236afa1353613fc99ed881e1c322
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-01-23 15:46:27 +0100

    Update benchmark

---
+++ jmh/src/main/java/com/github/skjolber/packing/jmh/ep/ExtremePoint3DEntry.java
@@ -0,0 +1,23 @@
+package com.github.skjolber.packing.jmh.ep;
+
+import com.github.skjolber.packing.ep.points3d.DefaultPlacement3D;
+
+public class ExtremePoint3DEntry {
+
+	private final int index;
+	private final DefaultPlacement3D placement;
+
+	public ExtremePoint3DEntry(int index, DefaultPlacement3D placement) {
+		super();
+		this.index = index;
+		this.placement = placement;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+	
+	public DefaultPlacement3D getPlacement() {
+		return placement;
+	}
+}
---
+++ jmh/src/main/java/com/github/skjolber/packing/jmh/ep/ExtremePoints2DEntries.java
@@ -0,0 +1,29 @@
+package com.github.skjolber.packing.jmh.ep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.skjolber.packing.ep.points2d.ExtremePoints2D;
+
+public class ExtremePoints2DEntries {
+
+	private final List<ExtremePoint2DEntry> entries = new ArrayList<>();
+
+	private final ExtremePoints2D extremePoints2D;
+	
+	public ExtremePoints2DEntries(ExtremePoints2D extremePoints2D) {
+		this.extremePoints2D = extremePoints2D;
+	}
+	
+	public void add(ExtremePoint2DEntry entry) {
+		entries.add(entry);
+	}
+	
+	public List<ExtremePoint2DEntry> getEntries() {
+		return entries;
+	}
+	
+	public ExtremePoints2D getExtremePoints2D() {
+		return extremePoints2D;
+	}
+}
