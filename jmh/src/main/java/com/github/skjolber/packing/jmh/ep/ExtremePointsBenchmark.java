f5f05639d71f758f75ae8be19444544125c7bca1
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-01-23 15:37:46 +0100

    format

--- jmh/src/main/java/com/github/skjolber/packing/jmh/ep/ExtremePointsBenchmark.java
+++ jmh/src/main/java/com/github/skjolber/packing/jmh/ep/ExtremePointsBenchmark.java
@@ -23,56 +23,56 @@ import com.github.skjolber.packing.ep.points3d.ExtremePoints3D;
 @Measurement(iterations = 1, time = 30, timeUnit = TimeUnit.SECONDS)
 public class ExtremePointsBenchmark {
 
-    @Benchmark
-    public int points2D(ExtremePoints2DState state) throws Exception {
-    	int size = 0;
-    	List<ExtremePoints2DEntries> entries = state.getEntries();
-    	for(ExtremePoints2DEntries e : entries) {
-    		ExtremePoints2D extremePoints2D = e.getExtremePoints2D();
-    		
-    		for (ExtremePoint2DEntry extremePointEntry : e.getEntries()) {
+	@Benchmark
+	public int points2D(ExtremePoints2DState state) throws Exception {
+		int size = 0;
+		List<ExtremePoints2DEntries> entries = state.getEntries();
+		for(ExtremePoints2DEntries e : entries) {
+			ExtremePoints2D extremePoints2D = e.getExtremePoints2D();
+			
+			for (ExtremePoint2DEntry extremePointEntry : e.getEntries()) {
 				extremePoints2D.add(extremePointEntry.getIndex(), extremePointEntry.getPlacement());
 			}
-    		size += extremePoints2D.getValueCount();
-    		
-    		extremePoints2D.redo();
-    	}
-    	
-    	return size;
-    }
+			size += extremePoints2D.getValueCount();
+			
+			extremePoints2D.redo();
+		}
+		
+		return size;
+	}
 
-    @Benchmark
-    public int points3D(ExtremePoints3DState state) throws Exception {
-    	int size = 0;
-    	List<ExtremePoints3DEntries> entries = state.getEntries();
-    	for(ExtremePoints3DEntries e : entries) {
-    		ExtremePoints3D extremePoints3D = e.getExtremePoints3D();
-    		
-    		for (ExtremePoint3DEntry extremePointEntry : e.getEntries()) {
+	@Benchmark
+	public int points3D(ExtremePoints3DState state) throws Exception {
+		int size = 0;
+		List<ExtremePoints3DEntries> entries = state.getEntries();
+		for(ExtremePoints3DEntries e : entries) {
+			ExtremePoints3D extremePoints3D = e.getExtremePoints3D();
+			
+			for (ExtremePoint3DEntry extremePointEntry : e.getEntries()) {
 				extremePoints3D.add(extremePointEntry.getIndex(), extremePointEntry.getPlacement());
 			}
-    		size += extremePoints3D.getValueCount();
-    		
-    		extremePoints3D.redo();
-    	}
-    	
-    	return size;
-    }
+			size += extremePoints3D.getValueCount();
+			
+			extremePoints3D.redo();
+		}
+		
+		return size;
+	}
 
-    
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(ExtremePointsBenchmark.class.getSimpleName())
-                .mode(Mode.Throughput)
-                /*
-                .forks(1)
-                .measurementIterations(1)
-                .measurementTime(TimeValue.seconds(15))
-                .timeout(TimeValue.seconds(10))
-                */
-                .build();
+	
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(ExtremePointsBenchmark.class.getSimpleName())
+				.mode(Mode.Throughput)
+				/*
+				.forks(1)
+				.measurementIterations(1)
+				.measurementTime(TimeValue.seconds(15))
+				.timeout(TimeValue.seconds(10))
+				*/
+				.build();
 
-        new Runner(opt).run();
-    }    
+		new Runner(opt).run();
+	}
 
 }
