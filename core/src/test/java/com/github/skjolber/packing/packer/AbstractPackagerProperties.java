e95fcc2544964eca1d94ca3e7ea4a7da953e0688
Michel Daviot <michel.daviot@colisweb.com>
2022-04-07 09:56:25 +0200

    refactoring

--- core/src/test/java/com/github/skjolber/packing/packer/AbstractPackagerProperties.java
+++ core/src/test/java/com/github/skjolber/packing/packer/AbstractPackagerProperties.java
@@ -5,17 +5,37 @@ import com.github.skjolber.packing.api.Container;
 import com.github.skjolber.packing.api.Dimension;
 import com.github.skjolber.packing.api.StackableItem;
 import com.github.skjolber.packing.impl.ValidatingStack;
+import com.github.skjolber.packing.packer.bruteforce.BruteForcePackager;
+import com.github.skjolber.packing.packer.bruteforce.FastBruteForcePackager;
+import com.github.skjolber.packing.packer.bruteforce.ParallelBruteForcePackager;
+import com.github.skjolber.packing.packer.laff.LargestAreaFitFirstPackager;
+import com.github.skjolber.packing.packer.plain.PlainPackager;
 import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 import static java.util.Collections.singletonList;
 
 @RunWith(JUnitQuickcheck.class)
-public abstract class AbstractPackagerProperties extends AbstractPackagerTest {
-	protected abstract AbstractPackager<?,?> buildPackager(Container container);
+public class AbstractPackagerProperties extends AbstractPackagerTest {
+
+	Function<Container, AbstractPackager<?, ?>> plainPackager =
+		container -> PlainPackager.newBuilder().withContainers(container).build();
+	Function<Container, AbstractPackager<?, ?>> bruteForcePackager =
+		container -> BruteForcePackager.newBuilder().withContainers(container).build();
+	Function<Container, AbstractPackager<?, ?>> fastBruteForcePackager =
+		container -> FastBruteForcePackager.newBuilder().withContainers(container).build();
+	Function<Container, AbstractPackager<?, ?>> parallelBruteForcePackager =
+		container -> ParallelBruteForcePackager.newBuilder().withContainers(container).build();
+	Function<Container, AbstractPackager<?, ?>> largestAreaFitFirstPackager =
+		container -> LargestAreaFitFirstPackager.newBuilder().withContainers(container).build();
+
 
 	@Property
 	public void eightBoxes2x2x2(@From(DimensionGenerator.class) Dimension boxSize,
@@ -29,7 +49,9 @@ public abstract class AbstractPackagerProperties extends AbstractPackagerTest {
 			2 * boxSize.getDy() + yVariation,
 			2 * boxSize.getDz() + zVariation);
 
-		runTest(containerSize, boxSize, count
+		runTest(containerSize, boxSize, count, bruteForcePackager,
+			fastBruteForcePackager,
+			parallelBruteForcePackager
 		);
 	}
 
@@ -44,29 +66,38 @@ public abstract class AbstractPackagerProperties extends AbstractPackagerTest {
 			2 * boxSize.getDy() + yVariation,
 			2 * boxSize.getDz() + zVariation);
 
-		runTest(containerSize, boxSize, count
+		runTest(containerSize, boxSize, count, bruteForcePackager,
+			fastBruteForcePackager,
+			parallelBruteForcePackager,
+			plainPackager
 		);
+
 	}
 
-	private void runTest(final Dimension containerSize, final Dimension boxSize, final int count) {
-		final Container container = Container.newBuilder()
-			.withDescription(containerSize.encode())
-			.withEmptyWeight(0)
-			.withSize(containerSize.getDx(), containerSize.getDy(), containerSize.getDz())
-			.withMaxLoadWeight(count)
-			.withStack(new ValidatingStack())
-			.build();
-		AbstractPackager<?, ?> packager = buildPackager(container);
+	private void runTest(final Dimension containerSize,
+											 final Dimension boxSize,
+											 final int count,
+											 Function<Container, AbstractPackager<?, ?>>... packagers) {
+		for (final Function<Container, AbstractPackager<?, ?>> packagerBuilder : packagers) {
+			final Container container = Container.newBuilder()
+				.withDescription(containerSize.encode())
+				.withEmptyWeight(0)
+				.withSize(containerSize.getDx(), containerSize.getDy(), containerSize.getDz())
+				.withMaxLoadWeight(count)
+				.withStack(new ValidatingStack())
+				.build();
 
-		final Box box = Box.newBuilder()
-			.withDescription(boxSize.encode())
-			.withRotate3D()
-			.withSize(boxSize.getDx(), boxSize.getDy(), boxSize.getDz())
-			.withWeight(1)
-			.build();
-		Container fits = packager.pack(singletonList(new StackableItem(box, count)));
-		assertValid(fits);
+			final AbstractPackager<?, ?> packager = packagerBuilder.apply(container);
+
+			final Box box = Box.newBuilder()
+				.withDescription(boxSize.encode())
+				.withRotate3D()
+				.withSize(boxSize.getDx(), boxSize.getDy(), boxSize.getDz())
+				.withWeight(1)
+				.build();
+			Container fits = packager.pack(singletonList(new StackableItem(box, count)));
+			assertValid(fits);
+		}
 	}
-
 
 }
