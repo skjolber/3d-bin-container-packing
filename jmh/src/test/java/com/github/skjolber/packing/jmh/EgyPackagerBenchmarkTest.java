61491741174a5a34d171e59d5c2e9691f80e7503
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-07-13 19:36:30 +0200

    Disable test

--- jmh/src/test/java/com/github/skjolber/packing/jmh/EgyPackagerBenchmarkTest.java
+++ jmh/src/test/java/com/github/skjolber/packing/jmh/EgyPackagerBenchmarkTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class EgyPackagerBenchmarkTest {
@@ -19,6 +20,7 @@ public class EgyPackagerBenchmarkTest {
 	}
 	
 	@Test
+	@Disabled
     public void parallelPackager() throws Exception {
     	assertValid(state.getParallelBruteForcePackager(), Long.MAX_VALUE);
     }
