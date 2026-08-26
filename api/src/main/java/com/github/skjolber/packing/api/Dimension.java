2d60de5f2aa5e5366ba4fd65610d8f0ed1849979
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2026-08-26 21:42:46 +0200

    Remove unused code

--- api/src/main/java/com/github/skjolber/packing/api/Dimension.java
+++ api/src/main/java/com/github/skjolber/packing/api/Dimension.java
@@ -1,15 +1,7 @@
 package com.github.skjolber.packing.api;
 
-import java.util.Set;
-
 public enum Dimension {
 	
 	X, Y, Z;
-	
-	public static Set<Dimension> MOVE_XY = Set.of(X, Y); // i.e. horizontally
-	public static Set<Dimension> MOVE_ALL = Set.of(X, Y, Z);
-	public static Set<Dimension> MOVE_X = Set.of(X);
-	public static Set<Dimension> MOVE_Y = Set.of(Y);
-	public static Set<Dimension> MOVE_Z = Set.of(Z);
 	
 }
